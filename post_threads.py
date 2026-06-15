07cf41026bf193088d250f9ebb64922843095ded
sehatguyonan-coder <sehatguyonan@gmail.com>
2026-06-15 17:04:17 +0700

    Update post_threads.py

--- post_threads.py
+++ post_threads.py
@@ -4,12 +4,17 @@ import requests
 ACCESS_TOKEN = os.getenv("THREADS_ACCESS_TOKEN")
 USER_ID = os.getenv("THREADS_USER_ID")
 
-MESSAGE = "Halo Threads! Ini posting otomatis dari GitHub Actions 🚀"
+MESSAGE = "Ini posting gambar otomatis dari GitHub Actions 🚀"
 
+IMAGE_URL = "https://raw.githubusercontent.com/sehatguyonan-coder/BOT-THREADS-YANG-TERBARU/main/15.png"
+
+
+# 1. Buat media gambar
 create_url = f"https://graph.threads.net/v1.0/{USER_ID}/threads"
 
 create_data = {
-    "media_type": "TEXT",
+    "media_type": "IMAGE",
+    "image_url": IMAGE_URL,
     "text": MESSAGE,
     "access_token": ACCESS_TOKEN
 }
@@ -20,6 +25,8 @@ result = response.json()
 print("Create response:")
 print(result)
 
+
+# 2. Publish gambar
 if "id" in result:
     creation_id = result["id"]
 
@@ -30,13 +37,10 @@ if "id" in result:
         "access_token": ACCESS_TOKEN
     }
 
-    publish = requests.post(
-        publish_url,
-        data=publish_data
-    )
+    publish = requests.post(publish_url, data=publish_data)
 
     print("Publish response:")
     print(publish.json())
 
 else:
-    print("Gagal membuat draft Threads")
+    print("Gagal membuat posting gambar")
