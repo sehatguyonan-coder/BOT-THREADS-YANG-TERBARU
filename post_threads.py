806304a9d574b604aee7f663b6b77bae8266923d
sehatguyonan-coder <sehatguyonan@gmail.com>
2026-06-15 16:47:27 +0700

    Update post_threads.py

--- post_threads.py
+++ post_threads.py
@@ -2,14 +2,41 @@ import os
 import requests
 
 ACCESS_TOKEN = os.getenv("THREADS_ACCESS_TOKEN")
+USER_ID = os.getenv("THREADS_USER_ID")
 
-url = "https://graph.threads.net/v1.0/me"
+MESSAGE = "Halo Threads! Ini posting otomatis dari GitHub Actions 🚀"
 
-params = {
-    "fields": "id,username",
+create_url = f"https://graph.threads.net/v1.0/{USER_ID}/threads"
+
+create_data = {
+    "media_type": "TEXT",
+    "text": MESSAGE,
     "access_token": ACCESS_TOKEN
 }
 
-response = requests.get(url, params=params)
+response = requests.post(create_url, data=create_data)
+result = response.json()
 
-print(response.json())
+print("Create response:")
+print(result)
+
+if "id" in result:
+    creation_id = result["id"]
+
+    publish_url = f"https://graph.threads.net/v1.0/{USER_ID}/threads_publish"
+
+    publish_data = {
+        "creation_id": creation_id,
+        "access_token": ACCESS_TOKEN
+    }
+
+    publish = requests.post(
+        publish_url,
+        data=publish_data
+    )
+
+    print("Publish response:")
+    print(publish.json())
+
+else:
+    print("Gagal membuat draft Threads")
